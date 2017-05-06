3ebd2a240a6c3c1a0db4eb568834cab2824d8880
Jonathan Johansson <jonathan.b.johansson88@gmail.com>
2017-05-06 11:20:07 +0200

    Update quizlist

--- imports/ui/QuizList.jsx
+++ imports/ui/QuizList.jsx
@@ -1,32 +1,69 @@
 import React, { Component } from 'react';
-
+import moment from 'moment';
 import Quiz from './Quiz.jsx';
 
+
 export default class QuizList extends Component {
-    getQuizes() {
-        return [
-            { _id: 1, name: "Ett quiz", place: "Din mammas källare", date: "2018-12-31 13:30", groupCount: 3, quizMaster: "Din mamma"}
-        ]
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentDate: new Date()
+        };
+        this.nextDay = this.nextDay.bind(this);
+        this.previousDay = this.previousDay.bind(this);
     }
 
-    renderQuizes() {
-        let quizes = this.getQuizes();
+    getQuizesByDate(date) {
+        if (this.isToday()) {
+            return [
+                {_id: 1, name: "Ett quiz", place: "Din mammas källare", date: "2018-12-31 13:30", groupCount: 3, quizMaster: "Din mamma"}
+            ]
+        }
+        return [];
+    }
 
+    renderQuizes(quizes) {
+        if (quizes.length === 0) {
+            return (<p>Tyvärr ingen quiz idag</p>);
+        }
         return quizes.map((quiz) => (
-            <Quiz key={quiz._id} quiz={quiz} />
-        ));
+                    <Quiz key={quiz._id} quiz={quiz} />
+                    ));
+    }
+
+    renderPrevious() {
+        return this.isToday() || (<button onClick={this.previousDay}>&lt;-</button>)
+    }
+
+    isToday() {
+        return moment(this.state.currentDate).isSame(new Date(), 'day');
+    }
+
+    nextDay() {
+        this.setState({currentDate: moment(this.state.currentDate).add({day: 1}).toDate()});
+    }
+
+    previousDay() {
+        this.setState({currentDate: moment(this.state.currentDate).subtract({day: 1}).toDate()});
     }
 
     render() {
+        let tmp = Geolocation.currentLocation();
+        let quizes = this.getQuizesByDate(this.state.currentDate);
+//        console.log(tmp);
+
         return (
-            <div className="container">
-                <header>
-                    Quizes!
-                </header>
-                <ul>
-                    {this.renderQuizes()}
-                </ul>
-            </div>
-        );
+                <div className="container">
+                    <header>
+                        {this.renderPrevious()}
+                        {moment(this.state.currentDate).format('YYYY-MM-DD')}
+                        <button onClick={this.nextDay}>-&gt;</button>
+                    </header>
+                    <ul>
+                        {this.renderQuizes(quizes)}
+                    </ul>
+                </div>
+                );
     }
 }
